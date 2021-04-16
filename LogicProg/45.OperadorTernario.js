2fe6a5940c5c5b83252e5203c8d7aec72bb79636
Davi Costa <daviita1@hotmail.com>
2021-04-16 16:09:17 -0300

    Lesson 45 finished

---
+++ LogicProg/45.OperadorTernario.js
@@ -0,0 +1,9 @@
+
+//condição ? valor para vertdadeiro : valor para falso
+
+
+const point = 5000
+const levelUser = point >= 1000 ? 'Vip' : 'Standard'
+
+const corUser = null
+const corPadrão = corUser || 'Blue'
