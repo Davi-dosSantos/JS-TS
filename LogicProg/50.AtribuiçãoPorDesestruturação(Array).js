0de805762a8d60da7b0d19b5ddd7b34fa201a699
Davi Costa <daviita1@hotmail.com>
2021-04-24 21:54:37 -0300

    desestruturação aula 50 array

--- LogicProg/50.AtribuiçãoPorDesestruturação(Array).js
+++ LogicProg/50.AtribuiçãoPorDesestruturação(Array).js
@@ -1,5 +1,19 @@
-let a = a
-let b = a
-let c = c
+// let a = 'a'
+// let b = 'b'
+// let c = 'c'
 
-[a, b, c] = [c, b, a]
+// [a, b, c] = [c, b, a]
+// console.log(a, b, c)
+
+////////... rest , ... spread
+
+const numbers = [1, 2, 3, 5, 6, 34, 12354, 34243, 23, 21, 122]
+const [um, dois, , , cinco, ...rest] = numbers
+console.log(cinco)
+
+
+//////////desestruturando
+
+const lista = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+const [lista1, lista2, lista3, lista4] = lista
+console.log(lista3[2])
