0b4d461ccb9d16ae21ecf4296db2ff09e2f2dba3
Davi Costa <daviita1@hotmail.com>
2021-05-02 10:35:09 -0300

    leason 64 try catch finish

--- LogicProg/64.tryCatchFinally.js
+++ LogicProg/64.tryCatchFinally.js
@@ -1,9 +1,34 @@
+// try {
+//     console.log('abri um arquivo');
+//     console.log('Manipulei o arquivo e gerou erro');
+//     console.log('fechei o arquivo')
+// } catch (e) {
+//     console.log('tratando o erro')
+// } finally {
+//     console.log('Finally')
+// }
+
+function retornaHora(data) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instancia de Date. ');
+    }
+    if (!data) {
+        data = new Date();
+    }
+    return data.toLocaleDateString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
+}
 try {
-    console.log('abri um arquivo');
-    console.log('Manipulei o arquivo e gerou erro');
-    console.log('fechei o arquivo')
+    const data = new Date('01-01-1970 12:26:78')
+    const hora = retornaHora(11);
+    console.log(hora);
 } catch (e) {
-    console.log('tratando o erro')
+    console.log(Error(e));
+    //tratar error
 } finally {
-    console.log('Finally')
+    console.log('Tenha um bom dia.')
 }
