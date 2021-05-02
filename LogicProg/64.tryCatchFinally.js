f1596242655f0ccfcbd17b6b154531ef7c1555e5
Davi Costa <daviita1@hotmail.com>
2021-05-02 10:22:01 -0300

    leason 63 try catch finish

---
+++ LogicProg/64.tryCatchFinally.js
@@ -0,0 +1,9 @@
+try {
+    console.log('abri um arquivo');
+    console.log('Manipulei o arquivo e gerou erro');
+    console.log('fechei o arquivo')
+} catch (e) {
+    console.log('tratando o erro')
+} finally {
+    console.log('Finally')
+}
