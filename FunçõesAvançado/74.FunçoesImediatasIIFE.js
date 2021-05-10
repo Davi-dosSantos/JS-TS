44b279752b41e81eeecbd764e7f65397c4806fef
Davi Costa <daviita1@hotmail.com>
2021-05-09 22:45:43 -0300

    leason 74 funções imediatas finish

--- FunçõesAvançado/74.FunçoesImediatasIIFE.js
+++ FunçõesAvançado/74.FunçoesImediatasIIFE.js
@@ -1,1 +1,15 @@
 //IIFE -> immediately invoked function expression
+
+(function (idade, peso, altura) {
+    const sobrenome = 'Miranda';
+    function criaNome(nome) {
+        return nome + " " + sobrenome;
+    }
+
+    function falaNome() {
+        console.log(criaNome('Davi'))
+    }
+
+    falaNome();
+    console.log(idade, peso, altura)
+})(30, 100, 194);
