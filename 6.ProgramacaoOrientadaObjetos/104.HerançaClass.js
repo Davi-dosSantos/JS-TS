bc3999fad2a2c8dd84ef857bfeb73d0a46bdc230
Davi Costa <daviita1@hotmail.com>
2021-05-16 17:04:45 -0300

    leason 104.2 finish

--- 6.ProgramacaoOrientadaObjetos/104.HerançaClass.js
+++ 6.ProgramacaoOrientadaObjetos/104.HerançaClass.js
@@ -28,5 +28,19 @@ class Smartphone extends DispositivoEletronico {
     }
 }
 
+class Tablet extends DispositivoEletronico {
+    constructor(nome, wifi) {
+        super(nome);
+        this.wifi = wifi;
+    }
+    ligar() {
+        console.log(`Voce alterou o metodo ligar`)
+    }
+}
+
 const s1 = new Smartphone('iPhone', 'Preto', '10S');
 console.log(s1);
+
+const t1 = new Tablet('iPad', true);
+t1.ligar();
+console.log(t1);
