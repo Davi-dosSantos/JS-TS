e34708cdfc5ec24b5d82a206a73dbc9d56cf7394
Davi Costa <daviita1@hotmail.com>
2021-05-16 17:00:51 -0300

    leason 104 finish

---
+++ 6.ProgramacaoOrientadaObjetos/104.HerançaClass.js
@@ -0,0 +1,32 @@
+class DispositivoEletronico {
+    constructor(nome) {
+        this.nome = nome;
+        this.ligador = false;
+    }
+
+    ligar() {
+        if (this.ligado) {
+            console.log(`${this.nome} já ligado`);
+            return;
+        }
+        this.ligado = true;
+    }
+    desligar() {
+        if (!this.ligado) {
+            console.log(`${this.nome} já desligado`)
+            return
+        }
+        this.ligado = false;
+    }
+}
+
+class Smartphone extends DispositivoEletronico {
+    constructor(nome, cor, modelo) {
+        super(nome);
+        this.cor = cor;
+        this.modelo = modelo;
+    }
+}
+
+const s1 = new Smartphone('iPhone', 'Preto', '10S');
+console.log(s1);
